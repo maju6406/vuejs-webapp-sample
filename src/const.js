b2b06d7e742b48cfa0a9467939f9ca7a056d4670
Abir Majumdar <beersy@gmail.com>
2021-02-19 00:13:24 +0000

    Added newrelic

--- src/const.js
+++ src/const.js
@@ -1,6 +1,6 @@
 
 export const AppInfo = {
   AppName: "vuejs-webapp-sample",
-  AppDeveloper: "ybkuroki",
-  GithubLink: "https://github.com/ybkuroki/vuejs-webapp-sample"
+  AppDeveloper: "maju6406",
+  GithubLink: "https://github.com/maju6406/vuejs-webapp-sample"
 };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+require('newrelic');
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
@@ -8,6 +9,7 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 import '@fortawesome/fontawesome-free/js/all.min.js'
+
 
 Vue.use(VueMaterial)
 
